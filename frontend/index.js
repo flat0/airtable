3e1755cd0a42c16f1c898b92a9c48fb0bcb450ed
Dmitry Fedyuk <dfediuk@gmail.com>
2021-01-02 03:58:40 +0600

    no message

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,18 @@
-// 2021-01-02 https://airtable.com/developers/apps/guides/hello-world-tutorial
-import {initializeBlock} from '@airtable/blocks/ui';
+// 2021-01-02
+// 1) https://airtable.com/developers/apps/guides/hello-world-tutorial
+// 2) https://airtable.com/developers/apps/guides/to-do-list-tutorial
+import {initializeBlock, useBase, useRecords} from '@airtable/blocks/ui';
 import React from 'react';
-function HelloWorldApp() {return <div>A test Airtable app</div>;}
-initializeBlock(() => <HelloWorldApp />);
+function Main() {
+	// 2021-01-02 https://reactjs.org/docs/hooks-intro.html
+	const base = useBase();
+	const table = base.getTableByName('Tasks');
+	const records = useRecords(table);
+	return (
+		<div>
+			<div>{base.name} 2</div>
+			<div>Number of tasks: {records.length}</div>
+		</div>
+	);
+}
+initializeBlock(() => <Main />);
