e264b32344b76e1aad22af55b2d5acdf9d8d2071
Dmitry Fedyuk <dfediuk@gmail.com>
2021-01-02 06:24:15 +0600

    no message

--- frontend/index.js
+++ frontend/index.js
@@ -1,18 +1,14 @@
 // 2021-01-02
 // 1) https://airtable.com/developers/apps/guides/hello-world-tutorial
 // 2) https://airtable.com/developers/apps/guides/to-do-list-tutorial
-import {initializeBlock, useBase, useRecords} from '@airtable/blocks/ui';
+import {expandRecord, initializeBlock, TextButton, useBase, useRecords} from '@airtable/blocks/ui';
 import React from 'react';
 function Main() {
 	// 2021-01-02 https://reactjs.org/docs/hooks-intro.html
 	const base = useBase();
 	const table = base.getTableByName('Tasks');
 	const records = useRecords(table);
-	// 2021-01-02
-	// 1) «Since we're rendering a list, we include a unique key for each element by using the record's ID»:
-	// https://airtable.com/developers/apps/guides/to-do-list-tutorial#showing-the-name-of-the-records
-	// 2) «Lists and Keys»: https://reactjs.org/docs/lists-and-keys.html
-	const tasks = records.map(r => {return (<div key={r.id}>{r.name || 'Unnamed record'}</div>);});
+	const tasks = records.map(r => {return <Task key={r.id} r={r} />});
 	return (
 		<div>
 			<div>{base.name} 2</div>
@@ -21,4 +17,11 @@ function Main() {
 		</div>
 	);
 }
+// 2021-01-02
+// 1) «Since we're rendering a list, we include a unique key for each element by using the record's ID»:
+// https://airtable.com/developers/apps/guides/to-do-list-tutorial#showing-the-name-of-the-records
+// 2) «Lists and Keys»: https://reactjs.org/docs/lists-and-keys.html
+function Task({r}) {return (
+	<div>{r.name || 'Unnamed record'}</div>
+);}
 initializeBlock(() => <Main />);
